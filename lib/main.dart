41795a58bde1428087ee4ce5648ea7bec053f5d0
Ahmad-PC <ahmedkhan871871@gmail.com>
2022-06-27 22:37:37 -0700

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
   void setBrightness(value) async {
     value = value.round().toString();
     print("Here....");
-    print(value);
+    print(value);//
 
     final response =
         await http.get(Uri.parse('http://192.168.1.23/slider?value=${value}'));
